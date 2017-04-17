40811d16944c517d4f2464fafaeed2a7b866f17c
Reed Jessen <reed@algo.ventures>
2017-04-17 10:58:12 -0700

    Stubbed out TestPublicClient

--- GDAX/Tests.py
+++ GDAX/Tests.py
@@ -1,1 +1,36 @@
 import unittest
+import GDAX.PublicClient
+
+class TestPublicClient(unittest.TestCase):
+    def setUp(self):
+        publicClient = GDAX.PublicClient()
+
+    def testGetProducts(self):
+        pass
+
+    def testProductOrderBook(self):
+        pass
+
+    def testProductTicker(self):
+        pass
+
+    def testProductTrades(self):
+        pass
+
+    def testProductHistoricalRates(self):
+        pass
+
+    def testgetProduct24HrStats(self):
+        pass
+
+    def testgetCurrencies(self):
+        pass
+
+    def testgetTime(self):
+        pass
+
+class TestAuthenticatedClient(unittest.TestCase):
+    pass
+
+class TestWebSocketClient(unittest.TestCase):
+    pass
